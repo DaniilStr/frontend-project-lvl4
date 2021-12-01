a6148dd10103f216861eea0d5de9370dbc4da17e
Daniil Strasser <dajka444@gmail.com>
2021-12-01 18:17:28 +0800

    toast.error('Ошибка соединения') 7

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -30,25 +30,21 @@ const Chat = () => {
 
   const [contentLoaded, setContentLoaded] = useState(false);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      const url = routes.data();
-      try {
-        const res = await axios.get(url, { headers: getAuthorizationHeader() });
-        dispatch(setInitialState(res.data));
-        socket.auth = { token: getToken() };
-        setContentLoaded(true);
-      } catch (e) {
-        if (e.isAxiosError) {
-          auth.logOut();
-          return;
-        }
-
-        throw e;
+  useEffect(async () => {
+    const url = routes.data();
+    try {
+      const res = await axios.get(url, { headers: getAuthorizationHeader() });
+      dispatch(setInitialState(res.data));
+      socket.auth = { token: getToken() };
+      setContentLoaded(true);
+    } catch (e) {
+      if (e.isAxiosError) {
+        auth.logOut();
+        return;
       }
-    };
 
-    fetchData();
+      throw e;
+    }
   }, []);
 
   return contentLoaded ? (
