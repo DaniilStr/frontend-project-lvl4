0317e8a9c289c06bc61f36af951fc6d6c7449063
Daniil Strasser <dajka444@gmail.com>
2021-12-03 22:59:29 +0800

    Linter's remarks

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -30,23 +30,27 @@ const Chat = ({ toast }) => {
 
   const [contentLoaded, setContentLoaded] = useState(false);
 
-  useEffect(async () => {
-    const url = routes.data();
-    try {
-      const res = await axios.get(url, { headers: getAuthorizationHeader() });
-      dispatch(setInitialState(res.data));
-      socket.auth = { token: getToken() };
-      setContentLoaded(true);
-    } catch (e) {
-      if (e.isAxiosError) {
-        toast.error(t('errors.netError'));
-        auth.logOut();
-        return;
-      }
-
-      throw e;
-    }
-  }, []);
+  useEffect(
+    () => {
+      const fetchData = async () => {
+        const url = routes.data();
+        try {
+          const res = await axios.get(url, { headers: getAuthorizationHeader() });
+          dispatch(setInitialState(res.data));
+          socket.auth = { token: getToken() };
+          setContentLoaded(true);
+        } catch (e) {
+          if (e.isAxiosError) {
+            toast.error(t('errors.netError'));
+            auth.logOut();
+            return;
+          }
+          throw e;
+        }
+      };
+      fetchData();
+    },
+  );
 
   return contentLoaded ? (
     <div className="container h-100 my-4 overflow-hidden rounded shadow">
